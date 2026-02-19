ab629ffefcf03dc695df49a901a94b68bcb7ed56
Suryansh Bhupesh Purohit <spurohi9@asu.edu>
2026-02-18 19:40:09 -0700

    #104: Added auth context

---
+++ frontend/context/AuthContext.js
@@ -0,0 +1,31 @@
+import React, { createContext, useEffect, useState, useContext } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../config/firebase";
+
+export const AuthContext = createContext();
+
+export function AuthProvider({ children }) {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (firebaseUser) => {
+      setUser(firebaseUser);
+      setLoading(false);
+      console.log("=====called",firebaseUser, loading );
+      
+    });
+
+    return unsubscribe;
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
